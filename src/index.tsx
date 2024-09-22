9ff58cb1547f9aaf512db7747c35a4577c9b0a30
EgorUU <egorsairanov12@gmail.com>
2024-09-22 12:52:05 +0300

    second commit

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter/*  basename={process.env.PUBLIC_URL} */>
+    <BrowserRouter basename={process.env.PUBLIC_URL} >
       <Provider store={store}>
         <App />
       </Provider>
